f24b83e85ac0d2b5de4557419d356d7b17fc99f6
kzefram <karenabourgeois@gmail.com>
2024-06-27 17:43:26 -0300

    initial commit - boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-red-700">
+      Hello world!
+    </h1>
+  )
+}
