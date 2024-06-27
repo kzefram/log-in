90f76f5516f650f64abb68ff2788fe4d6e5bf1a9
kzefram <karenabourgeois@gmail.com>
2024-06-27 18:29:49 -0300

    Added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { BrowswerRouter, Routers, Route } from 'react-router-dom';
 import './index.css';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-700">
-      Hello world!
-    </h1>
+    <BrowswerRouter>
+      <Routers>
+        <Route path="/" element={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/sign-in" component={Signin} />
+        <Route path='/sign-up' component= {Signup} />
+      </Routers>
+    </BrowswerRouter>
   )
 }
