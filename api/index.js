68d82b7af959af49ce66809ffada97aaf262a6bc
kzefram <karenabourgeois@gmail.com>
2024-07-03 20:03:24 -0300

    started a user.route file

--- api/index.js
+++ api/index.js
@@ -14,8 +14,18 @@ mongoose
         console.log('Error connecting to MongoDB');
     });
 
+// Creating Sever connection
+
 const app = express();
 
 app.listen(4000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 4000');
     });
+
+// Creating a route
+
+app.get('/', (req, res) => {
+    res.json({ 
+        message: 'API is working' 
+    });
+}); 
